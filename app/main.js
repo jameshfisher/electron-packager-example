617352415ca264e996e0ab1628957a7da84e5383
Jim Fisher <jameshfisher@gmail.com>
2020-10-13 15:59:25 +0100

    Sadly, electron-packager forces us into this situation

--- app/main.js
+++ app/main.js
@@ -1,3 +1,16 @@
 const answer = require('the-answer');
 
+// Demonstrate packaged dependency
 console.log(`The answer is: ${answer}`);
+
+const {app, BrowserWindow} = require('electron');
+
+(async() => {
+  await app.whenReady();
+  const window = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+  window.loadFile('./app/index.html');
+})();
--- build.js
+++ build.js
@@ -4,11 +4,20 @@ const packager = require("electron-packager");
 (async () => {
   const appPaths = await packager({
     // The default approach in Electron world seems to be to mix source files for distribution (e.g. main.js)
-    // with non-distributable files (e.g. code-signing certs!) in the same directory,
+    // with non-distributable files (e.g. super_secret.cert) in the same directory,
     // then ignore some with the "ignore" property below.
-    // This is obviously playing with fire!!
-    // We instead put files intended for distribution into app/
-    dir: 'app',
+    // This is obviously playing with fire,
+    // and the ideal would be to put our distributable app in a separate dir like 'app/'.
+    // But then electron-packager doesn't respect our package.json and bundle our node_modules.
+    // Instead, we use `ignore` with a function that turns it into a whitelist.
+    dir: '.',
+    ignore: path => {
+      console.log("Ignore path?", path);
+      if (path === "") return false;
+      if (path.startsWith("/node_modules")) return false;
+      if (path.startsWith("/app")) return false;
+      return true; // whitelist
+    },
     out: 'build',
     name: 'MyElectronApp',
     overwrite: true
